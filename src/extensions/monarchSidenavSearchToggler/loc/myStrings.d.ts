d551bbe03e6a4c40656666bd6a81cf65ea4bccc1
sharif <shahanur700@gmail.com>
2025-06-23 17:12:57 +0800

    first commit

---
+++ src/extensions/monarchSidenavSearchToggler/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IMonarchSidenavSearchTogglerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'MonarchSidenavSearchTogglerApplicationCustomizerStrings' {
+  const strings: IMonarchSidenavSearchTogglerApplicationCustomizerStrings;
+  export = strings;
+}
